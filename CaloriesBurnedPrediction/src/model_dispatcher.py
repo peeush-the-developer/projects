c89c79eeebcf917b8b26dab948c02b72959efe95
Peeush Agarwal <agarwal.peeush.developer@gmail.com>
2021-01-20 08:27:29 +0530

    Add RandomForestRegressor as new model (estimator=30)

--- CaloriesBurnedPrediction/src/model_dispatcher.py
+++ CaloriesBurnedPrediction/src/model_dispatcher.py
@@ -5,8 +5,10 @@ Dictionary <Model_Name, Model>
 
 from sklearn.linear_model import LinearRegression
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
 
 models = {
     'lr': LinearRegression(),
-    'dt': DecisionTreeRegressor(random_state=42)
+    'dt': DecisionTreeRegressor(random_state=42),
+    'rfe': RandomForestRegressor(n_estimators=30, random_state=42)
 }
