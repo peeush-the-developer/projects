551409982306db97d3cd76efe79070022e845119
Peeush Agarwal <agarwal.peeush.developer@gmail.com>
2021-01-23 17:43:20 +0530

    Update load_data to echo to load the data from Kaggle

--- MNIST/src/load_data.sh
+++ MNIST/src/load_data.sh
@@ -1,2 +1,6 @@
-wget https://pjreddie.com/media/files/mnist_train.csv -P ../input
-wget https://pjreddie.com/media/files/mnist_test.csv -P ../input
+# This is an OLD method.
+# wget https://pjreddie.com/media/files/mnist_train.csv -P ../input
+# wget https://pjreddie.com/media/files/mnist_test.csv -P ../input
+
+# From Kaggle itself: https://www.kaggle.com/c/digit-recognizer/data
+echo 'Load the data from https://www.kaggle.com/c/digit-recognizer/data into input folder'
