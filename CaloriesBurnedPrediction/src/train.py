0c47b77cf0759065e5c85b9b152a06a293542b3f
Peeush Agarwal <agarwal.peeush.developer@gmail.com>
2021-01-23 08:31:17 +0530

    Use cols argument to get from user and train model on same

--- CaloriesBurnedPrediction/src/train.py
+++ CaloriesBurnedPrediction/src/train.py
@@ -11,11 +11,15 @@ from sklearn.feature_selection import SelectFromModel
 import config
 import model_dispatcher
 
-def run(fold, model_name, use_duration = False):
+def get_columns(df, cols):
+    return df[cols], config.COLUMNS[cols]
+
+def run(fold, model_name, cols):
     # Read the training data with folds
     df = pd.read_csv(config.TRAINING_FILE)
 
-    if not use_duration:
+    columns = config.COLUMNS[cols]
+    if len(columns) == 0 or 'Gender' in columns:
         # 'Gender' column: Since it has 2 values only, we can use LabelEncoder that converts categories 
         # into numeric by mapping 'Male':0, 'Female':1 or vice-versa
         lbl_enc = LabelEncoder()
@@ -30,17 +34,18 @@ def run(fold, model_name, use_duration = False):
     # Initialize the model
     model = model_dispatcher.models[model_name]
 
-    if not use_duration:
+    if len(columns) == 0: # this is SFM type => Select from model itself
         # As we know from our 'explore_data.ipynb' that only 'Duration', 'Heart_Rate' and 'Body_Temp' columns are relevant.
         # Let's take all columns to be able to get important features from SelectFromModel
-        x_train = df_train.drop(['Calories','kfold'], axis=1).values
+        drop_cols = ['Calories','kfold']
+        x_train = df_train.drop(drop_cols, axis=1).values
         y_train = df_train.Calories.values
 
         # Col_names
-        col_names = df_train.drop(['Calories','kfold'], axis=1).columns.tolist()
+        col_names = df_train.drop(drop_cols, axis=1).columns.tolist()
 
         # Similarly for validation
-        x_val = df_val.drop(['Calories','kfold'], axis=1).values
+        x_val = df_val.drop(drop_cols, axis=1).values
         y_val = df_val.Calories.values
         
         # Select from the model
@@ -56,23 +61,23 @@ def run(fold, model_name, use_duration = False):
         else:
             print(sel_col_names)
 
-        # Fit the model on training data
-        model.fit(x_train_transformed, y_train)
-
-        # Predict on validation data
-        preds = model.predict(x_val_transformed)
+        x_train_final = x_train_transformed
+        x_val_final = x_val_transformed
     else:
-        x_train = df_train[['Duration']].values
+        x_train = df_train[columns].values
         y_train = df_train.Calories.values
 
-        x_val = df_val[['Duration']].values
+        x_val = df_val[columns].values
         y_val = df_val.Calories.values
         
-        # Fit the model on training data
-        model.fit(x_train, y_train)
+        x_train_final = x_train
+        x_val_final = x_val
+    
+    # Fit the model on training data
+    model.fit(x_train_final, y_train)
 
-        # Predict on validation data
-        preds = model.predict(x_val)
+    # Predict on validation data
+    preds = model.predict(x_val_final)
 
     # Calculate the RMSE for the model
     rmse = np.sqrt(mean_squared_error(y_val, preds))
@@ -80,12 +85,12 @@ def run(fold, model_name, use_duration = False):
     # Calculate the R2 score for the model
     r2 = r2_score(y_val, preds)
     
-    print(f'Model={model_name}, Fold={fold} => RMSE={rmse:.3f}, R2={r2:.3f}')
+    print(f'Cols={cols}, Model={model_name}, Fold={fold} => RMSE={rmse:.3f}, R2={r2:.3f}')
 
     # save the model
     joblib.dump(
         model, 
-        os.path.join(config.MODEL_OUTPUT, f'{model_name}_{fold}.bin'))
+        os.path.join(config.MODEL_OUTPUT, f'{model_name}_{cols}_{fold}_{r2:.3f}.bin'))
 
 if __name__ == "__main__":
 
@@ -95,8 +100,9 @@ if __name__ == "__main__":
     # Add arguments that we expect to parse from CLI
     ap.add_argument('--fold', type=int, required=True, help='Fold value to run the training script')
     ap.add_argument('--model', type=str, required=True, help='Model name to run the training script')
+    ap.add_argument('--columns', type=str, required=True, help='Columns to run the training script ("DUR", "ALN", "ALL", or "SFM")')
 
     # Parse the arguments received
     args = ap.parse_args()
 
-    run(fold=args.fold, model_name=args.model, use_duration=args.model == 'lr')
+    run(fold=args.fold, model_name=args.model, cols=args.columns)
